867a75742e0fcd284291b8647b21792f07ee309d
Stefan Lahne <stefan.lahne@student.uni-halle.de>
2023-12-15 10:27:22 +0100

    add a template

---
+++ template.rs
@@ -0,0 +1,29 @@
+use std::time::Instant;
+
+fn main() {
+    let start_time = Instant::now();
+
+    let input = include_str!("input.txt");
+    let test = include_str!("test.txt");
+    println!("{}", part1(test));
+    println!("{}", part1(input));
+    let end_time = Instant::now();
+    println!("{:?}", end_time - start_time);
+
+    let start_time = Instant::now();
+    let input = include_str!("input.txt");
+    let test = include_str!("test.txt");
+    println!("{}", part2(test));
+    println!("{}", part2(input));
+
+    let end_time = Instant::now();
+    println!("{:?}", end_time - start_time);
+}
+
+fn part1(input: &str) -> u64 {
+    1
+}
+
+fn part2(input: &str) -> u64 {
+    2
+}
